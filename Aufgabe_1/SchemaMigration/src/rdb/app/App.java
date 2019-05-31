4cafdee3e9f8d1a95e2772ba1ff186115be1c717
Atilla Demir <demir.atilla1@fh-swf.de>
2019-05-31 23:22:18 +0200

    Aufgabe_1 mit gui und noch unvollständig 3

--- Aufgabe_1/SchemaMigration/src/rdb/app/App.java
+++ Aufgabe_1/SchemaMigration/src/rdb/app/App.java
@@ -1,86 +1,167 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package rdb.app;
 
+import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
+import rdb.data.DbConnection;
+import rdb.data.DbConnectionSingletonFactory;
 
 /**
  *
  * @author a
  */
-/*
 public class App {
-        
-    private final String[] typeTableNames = {"company_type","link_type","kind_type","info_type","role_type"};
-    private final String[] typeTableNamesRefTables = {"movie_companies","movie_link","title","person_info",
-            "movie_info_idx","movie_info","cast_info"};
-    private final String[] allTables={"company_type","link_type","kind_type","info_type","role_type",
-            "movie_companies","movie_link","title","person_info","movie_info_idx",
-            "movie_info","cast_info","char_name","company_name","name"}; 
     
-    ArrayList<Map<String,String>> list_TableAndTypeNames_from_UserChoice = new ArrayList<>();
+    private DbConnectionSingletonFactory dbConFactory;
+    private DbConnection dbConOracle;
+    private DbConnection dbConPostgres;
+    private DatabaseMetaData databaseMetaDataPG;
+    private DatabaseMetaData databaseMetaDataORCL;
     
-    private void createSQLQuery_with_TypeChoices(){
-        ArrayList<Map<String,String>> list_of_SQLQuery_to_typeRefTables = null;
-        Map<String,String> map;
-        for(String s: typeTableNamesRefTables){
-            map = new HashMap<String, String>();
-            map.put(s,getSQLQueryfortypeRefTables(s));
+    private final String[] allTables={"kind_type", "link_type", "info_type", "company_type", "role_type", 
+                                      "company_name", "name", "char_name", "title", "person_info", 
+                                      "movie_link","movie_info_idx", "movie_info", "movie_companies","cast_info"};
+    
+    public App(){
+        dbConFactory = DbConnectionSingletonFactory.getDbConnectionSingletonFactory();
+        dbConOracle = dbConFactory.getDbConnection("oracle");
+        dbConPostgres = dbConFactory.getDbConnection("postgres");
+        getMetaDataConnection();
+    }
+    
+    public void getMetaDataConnection(){
+        try {
+            databaseMetaDataORCL = dbConOracle.getConnection().getMetaData();
+        } catch (SQLException e) {
+            System.err.println("Die Verbindung zur Oracle Datenbank (DatabaseSchema) war nicht erfolgreich.");
+            e.printStackTrace();
+        }
+        try {
+            databaseMetaDataPG = dbConPostgres.getConnection().getMetaData();
+        } catch (SQLException e) {
+            System.err.println("Die Verbindung zur Postgres Datenbank (DatabaseSchema) war nicht erfolgreich.");
+            e.printStackTrace();
         }
     }
     
-    private String getSQLQueryfortypeRefTables(String  refTableName){
-        String SQL = "SELECT * from "+refTableName+" ";
-        ArrayList<String> list = null;
-        for(Map<String,String> map : list_TableAndTypeNames_from_UserChoice){
-            for(Map.Entry<String,String> entry : map.entrySet()){
-                if(entry.getKey().equals(refTableName))
-                    list.add(entry.getValue());
+    private boolean schemaTransfer() {
+        ResultSetMetaData rsmd= null;
+        
+        List<StringBuilder> listSB = new ArrayList<>();
+        StringBuilder temp;
+        
+        boolean success = false;
+        
+        for (int i = 0; i < allTables.length; i++) {
+            temp = new StringBuilder();
+            try(ResultSet rs = databaseMetaDataPG.getColumns(null,null, allTables[i], null);
+                    ResultSet rs2 = databaseMetaDataPG.getPrimaryKeys( "" , "" , allTables[i]);
+                    ResultSet rs3 = databaseMetaDataPG.getImportedKeys( "" , "" , allTables[i]);)
+            {
+                temp.append("CREATE TABLE "+allTables[i]+"("+"\n");
+                
+                while(rs.next()){
+                        temp.append(rs.getString("COLUMN_NAME")+"\t"+convertType_Postgres_Oracle(rs.getString("TYPE_NAME"),rs.getInt("COLUMN_SIZE"))+","+"\n");
+                }
+                
+                while(rs2.next()){
+                    temp.append("CONSTRAINT "+rs2.getString("PK_NAME")+" PRIMARY KEY"+"("+rs2.getString("COLUMN_NAME")+"),");
+                }
+                
+                while(rs3.next()){
+                    if(rs3.getString("FK_NAME")!=null)
+                        temp.append("\n"+"CONSTRAINT "+rs3.getString("FK_NAME")+" FOREIGN KEY"+"("+rs3.getString("FKCOLUMN_NAME")+")"
+                            +" REFERENCES "+rs3.getString("PKTABLE_NAME")+"("+rs3.getString("PKCOLUMN_NAME")+"),");
+                    else
+                        temp.append("\n"+"CONSTRAINT "+"fk_"+rs3.getString("FKCOLUMN_NAME")+" FOREIGN KEY"+"("+rs3.getString("FKCOLUMN_NAME")+")"
+                            +" REFERENCES "+rs3.getString("PKTABLE_NAME")+"("+rs3.getString("PKCOLUMN_NAME")+"),");
+                }
+                temp.deleteCharAt(temp.lastIndexOf(","));
+                temp.append(")");
+                listSB.add(temp);
+            }catch(SQLException ex){
+                ex.printStackTrace();
             }
         }
-        if(!list.isEmpty())
-            SQL = SQL.concat("WHERE "+getTableNameByValue_ForTypeTables(list.get(0))+" IN(SELECT id FROM ");
-            for(String s : list)
-                SQL = SQL.concat("WHERE person_role_id IN(SELECT id FROM role_type WHERE role = '"
-                    +cast_info_Role_type1+"' OR role = '"+cast_info_Role_type1+"')");
+        
+        for(StringBuilder temp2 : listSB){
+                System.out.println(temp2.toString()+"\n\n\n");
+            }
+            return success;
     }
     
-    private void get_List_Of_TypeValuesTableNames(){
-        list_TableAndTypeNames_from_UserChoice = new ArrayList<>();
-        Map<String,String> map = null;
-        for(String s : observableList){
-            map = new HashMap<String,String>();
-            map.put(getTableNameByValue_ForTypeTables(s), s);
-            for(Map.Entry<String,String> entry : map.entrySet())
-                System.out.println("Key: "+entry.getKey()+"\tValue"+entry.getValue());
-            list_TableAndTypeNames_from_UserChoice.add(map);
+    
+    public String convertType_Postgres_Oracle(String s, int size){
+        String string = "";
+        switch(s){
+            case "serial":
+                string = "number(10)";
+                break;
+            case "int4":
+                string = "number(10)";
+                break;
+            case "varchar":
+                string = "varchar2"+"("+size+")";
+                break;
+            case "text":
+                string = "clob";
+                break;
         }
+        return string;
     }
     
-    private int getRowCountFromResulSet(){
-        return 0;}
-
-    public String[] getTypeTableNames() {
-        return typeTableNames;
-    }
-
-    public String[] getTypeTableNamesRefTables() {
-        return typeTableNamesRefTables;
-    }
-
-    public String[] getAllTables() {
-        return allTables;
-    }
-
-    public void setList_TableAndTypeNames_from_UserChoice(ArrayList<Map<String, String>> list_TableAndTypeNames_from_UserChoice) {
-        this.list_TableAndTypeNames_from_UserChoice = list_TableAndTypeNames_from_UserChoice;
+    private List<String> berechneTransfer() {
+        List<String> list = new ArrayList<String>();
+        
+        
+        
+        return list;
     }
     
+    private boolean startTransfer() {
+        boolean success = false;
+        
+        return success;
+    }
     
+    private ArrayList<String> getItemsList_for_ChoiceBoxes(String tableName) {
+        ArrayList<String> list = null;  
+        ArrayList<Integer> list2 = new ArrayList<>(); 
+            try(ResultSet rs= dbConPostgres.getConnection().createStatement().executeQuery("SELECT * from "+tableName+" ");)
+            {
+                list=new ArrayList<>();
+                while(rs.next()){
+                    list.add(rs.getString(2));
+                }
+                list.add(0, "");
+           }catch(SQLException ex){
+               ex.printStackTrace();
+        }
+        return list;
+    }
     
+    public ArrayList<String> call_ItemsList_for_ChoiceBoxes(String tableName) {
+        return getItemsList_for_ChoiceBoxes(tableName);
+    }
+    
+    public boolean call_SchemaTransfer() {
+        return schemaTransfer();
+    }
+    
+    public List<String> call_berechneTransfer() {
+        return berechneTransfer();
+    }
+    
+    public boolean call_startTransfer() {
+        return startTransfer();
+    }
 }
-
-*/
--- Aufgabe_1/SchemaMigration/src/rdb/pres/mainWindow/MainWindowController.java
+++ Aufgabe_1/SchemaMigration/src/rdb/pres/mainWindow/MainWindowController.java
@@ -30,6 +30,7 @@ import javafx.stage.Stage;
 import rdb.RDBSchemaMigration;
 import rdb.data.DbConnection;
 import rdb.data.DbConnectionSingletonFactory;
+import rdb.app.App;
 
 /**
  * Default Window to be displayed after successful login
@@ -42,6 +43,7 @@ public class MainWindowController implements Initializable {
    //private final RDBSchemaMigration application = RDBSchemaMigration.getInstance();
     
     private RDBSchemaMigration application;
+    private App app;
     
     @FXML
     private ListView<String> listView_Auswahl_Transfer;
@@ -60,6 +62,8 @@ public class MainWindowController implements Initializable {
     @FXML
     private ChoiceBox<Integer> choiceBox_ProductionYear_2;
     @FXML
+    private Button schemanaKopieren;
+    @FXML
     private Button button_addToListView;
     @FXML
     private Button button_deleteFromListView;
@@ -72,20 +76,6 @@ public class MainWindowController implements Initializable {
     
     List<ChoiceBox<String>> listOfChoiceBoxes = new ArrayList<ChoiceBox<String>>();
     ObservableList<String> observableList = FXCollections.observableArrayList();
-    ArrayList<Map<String,String>> list_TableAndTypeNames_from_UserChoice = new ArrayList<>();
-    private final String[] typeTableNames = {"company_type","link_type","kind_type","info_type","role_type"};
-    private final String[] typeTableNamesRefTables = {"movie_companies","movie_link","title","person_info",
-                                                      "movie_info_idx","movie_info","cast_info"};
-    private final String[] allTables={"company_type","link_type","kind_type","info_type","role_type",
-                                       "movie_companies","movie_link","title","person_info","movie_info_idx",
-                                       "movie_info","cast_info","char_name","company_name","name"}; 
-    private String[] SQLQueryFromTypeTableRefs;
-    
-    private DbConnectionSingletonFactory dbConFactory;
-    private DbConnection dbConOracle;
-    private DbConnection dbConPostgres;
-    private DatabaseMetaData databaseMetaDataPG=null;
-    private DatabaseMetaData databaseMetaDataORCL=null;
     
     public void setApp(RDBSchemaMigration application) {
         this.application = application;
@@ -93,23 +83,25 @@ public class MainWindowController implements Initializable {
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        dbConFactory = DbConnectionSingletonFactory.getDbConnectionSingletonFactory();
-        dbConOracle = dbConFactory.getDbConnection("oracle");
-        dbConPostgres = dbConFactory.getDbConnection("postgres");
-        getMetaDataConnection();
+        app = new App();
         addItems_To_ChoiceBoxes();
     }
 
     //TODO implement the controller for the migration process
 
     @FXML
+    private void schemaTransfer() {
+        app.call_SchemaTransfer();
+    }
+    
+    @FXML
     private void berechneTransfer() {
-        
+        app.call_berechneTransfer();
     }
     
     @FXML
     private void startTransfer() {
-        
+        app.call_startTransfer();
     }
     @FXML
     private void deleteFromListView() {
@@ -142,123 +134,30 @@ public class MainWindowController implements Initializable {
         observableList=list;
         listView_Auswahl_Transfer.getItems().addAll(observableList);
     }
-    
-    /*private void createSQLQuery_with_TypeChoices(){
-        ArrayList<Map<String,String>> list_of_SQLQuery_to_typeRefTables = null;
-        Map<String,String> map;
-        for(String s: typeTableNamesRefTables){
-            map = new HashMap<String, String>();
-            map.put(s,getSQLQueryfortypeRefTables(s));
-        }
-    }*/
-    
-   /* private String getSQLQueryfortypeRefTables(String  refTableName){
-        String SQL = "SELECT * from "+refTableName+" ";
-        ArrayList<String> list = null;
-        for(Map<String,String> map : list_TableAndTypeNames_from_UserChoice){
-            for(Map.Entry<String,String> entry : map.entrySet()){
-                if(entry.getKey().equals(refTableName))
-                    list.add(entry.getValue());
-            }
-        }
-        if(!list.isEmpty())
-            SQL = SQL.concat("WHERE "+getTableNameByValue_ForTypeTables(list.get(0))+" IN(SELECT id FROM ");
-            for(String s : list)
-                SQL = SQL.concat("WHERE person_role_id IN(SELECT id FROM role_type WHERE role = '"
-                    +cast_info_Role_type1+"' OR role = '"+cast_info_Role_type1+"')");
-    }*/
-    
-    private void get_List_Of_TypeValuesTableNames(){
-        list_TableAndTypeNames_from_UserChoice = new ArrayList<>();
-        Map<String,String> map = null;
-        for(String s : observableList){
-            map = new HashMap<String,String>();
-            map.put(getTableNameByValue_ForTypeTables(s), s);
-            for(Map.Entry<String,String> entry : map.entrySet())
-                System.out.println("Key: "+entry.getKey()+"\tValue"+entry.getValue());
-            list_TableAndTypeNames_from_UserChoice.add(map);
-        }
-    }
-    
-    private int getRowCountFromResulSet(){
-        return 0;
-    }
-    
-    private String getTableNameByValue_ForTypeTables(String value){
-        String tableName = null;
-        for (int i = 0; i < typeTableNames.length; i++) {
-           try(ResultSet rs= dbConPostgres.getConnection().createStatement().executeQuery("SELECT * from "+typeTableNames[i]+" ");)
-            { 
-                 while(rs.next()){
-                    if(rs.getString(2).equals(value))
-                           return typeTableNames[i];
-                }
-            }catch(SQLException ex){
-                ex.printStackTrace();
-            }
-        }
-        return tableName;
-    }
-    
-    
-    
-    private void addItems_To_ChoiceBoxes(){
-        String[] typeTableNames = {"company_type","link_type","kind_type","info_type","role_type"};
-        ArrayList<String> list = null;  
-        ArrayList<Integer> list2 = new ArrayList<>(); 
-        for (int i = 0; i < typeTableNames.length; i++) {
-            try(ResultSet rs= dbConPostgres.getConnection().createStatement().executeQuery("SELECT * from "+typeTableNames[i]+" ");)
-            {
-                list=new ArrayList<>();
-                while(rs.next()){
-                    list.add(rs.getString(2));
-                }
-                list.add(0, "");
-                switch(i){
-                    case 0: choiceBox_company_type.getItems().addAll(list); 
-                            listOfChoiceBoxes.add(choiceBox_company_type);
-                            break;
-                    case 1: choiceBox_link_type.getItems().addAll(list);
-                            listOfChoiceBoxes.add(choiceBox_link_type);
-                            break;
-                    case 2: choiceBox_kind_type.getItems().addAll(list);
-                            listOfChoiceBoxes.add(choiceBox_kind_type);
-                            break;
-                    case 3: choiceBox_info_type.getItems().addAll(list); 
-                            listOfChoiceBoxes.add(choiceBox_info_type);
-                            break;
-                    case 4: choiceBox_role_type.getItems().addAll(list);
-                            listOfChoiceBoxes.add(choiceBox_role_type);
-                            break;
-                }
 
-            }catch(SQLException ex){
-                ex.printStackTrace();
-            }
-        }
-        for (int i = 2019; i > 1880; i--) {
-            list2.add(i);
-        }
-        choiceBox_ProductionYear_1.getItems().addAll(list2);
-        choiceBox_ProductionYear_2.getItems().addAll(list2);
+    private void addItems_To_ChoiceBoxes(){
+        List<Integer> list = new ArrayList<>();
         
-            
-    }
-    
-    public void getMetaDataConnection(){
-        try {
-            databaseMetaDataORCL = dbConOracle.getConnection().getMetaData();
-        } catch (SQLException e) {
-            System.err.println("Die Verbindung zur Oracle Datenbank (DatabaseSchema) war nicht erfolgreich.");
-            e.printStackTrace();
+        choiceBox_company_type.getItems().addAll(app.call_ItemsList_for_ChoiceBoxes("company_type")); 
+        listOfChoiceBoxes.add(choiceBox_company_type);
+        
+        choiceBox_link_type.getItems().addAll(app.call_ItemsList_for_ChoiceBoxes("link_type"));
+        listOfChoiceBoxes.add(choiceBox_link_type);
+        
+        choiceBox_kind_type.getItems().addAll(app.call_ItemsList_for_ChoiceBoxes("kind_type"));
+        listOfChoiceBoxes.add(choiceBox_kind_type);
+        
+        choiceBox_info_type.getItems().addAll(app.call_ItemsList_for_ChoiceBoxes("info_type")); 
+        listOfChoiceBoxes.add(choiceBox_info_type);
+        
+        choiceBox_role_type.getItems().addAll(app.call_ItemsList_for_ChoiceBoxes("role_type"));
+        listOfChoiceBoxes.add(choiceBox_role_type);
+         
+        for (int i = 2019; i > 1880; i--) {
+            list.add(i);
         }
-        try {
-            databaseMetaDataPG = dbConPostgres.getConnection().getMetaData();
-        } catch (SQLException e) {
-            System.err.println("Die Verbindung zur Postgres Datenbank (DatabaseSchema) war nicht erfolgreich.");
-            e.printStackTrace();
-        }
+        choiceBox_ProductionYear_1.getItems().addAll(list);
+        choiceBox_ProductionYear_2.getItems().addAll(list);
     }
-    
     
 }
